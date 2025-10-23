a16c2a4d5464f000770866140327327c12f95cf8
harshada-here <harshadahere07@gmail.com>
2025-10-23 12:24:59 +0530

    Add files via upload

---
+++ tutedude assignment 1.py
@@ -0,0 +1,16 @@
+#Task 1: Perform Basic Mathematical Operations
+
+x=int(input("Enter the first number: "))        #input of first number
+y=int(input("Enter the second number: "))       #input of secont number
+print("Addition :",x+y)     #perform addition
+print("Subtraction :",x-y)      #perform subtraction
+print("Multiplication :",x*y)       #perform multiplication
+print("Division:",x/y)       #perform division   
+
+
+#Task 2: Create a Personalized Greeting
+
+first_name=str(input("Enter your first name: "))
+last_name=str(input("Enter your last name: "))
+full_name= first_name + last_name
+print("Hello,",full_name,"! Welcome to the Python Program.")
